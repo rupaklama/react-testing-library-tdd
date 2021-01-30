5e9124afce64b73c8c976b9c89d37486c28ea539
Rupak Lama <rlama@uptopcorp.com>
2021-01-29 19:53:32 -0500

    unit testing

--- color-button-app/src/App.js
+++ color-button-app/src/App.js
@@ -2,28 +2,54 @@ import { useState } from 'react';
 
 import './App.css';
 
+// function
+export function replaceCamelWithSpaces(colorName) {
+  // regular expression - if there's a capital letter one or more in the middle of the word 
+  // proceeded by SPACE
+  return colorName.replace(/\B([A-Z])\B/g, ' $1');
+}
 
+console.log(replaceCamelWithSpaces('Red')); // Red
+console.log(replaceCamelWithSpaces('MidnightBlue')); // Midnight Blue
+console.log(replaceCamelWithSpaces('MediumVioletRed')); // Medium Violet Red
 
 function App() {
+  const [buttonColor, setButtonColor] = useState('red');
+  const [disabled, setDisabled] = useState(false);
 
-  const [ buttonColor, setButtonColor] = useState('red');
-
-  // if button color is red, set new button color to blue otherwise red
+  // if button color is red, set color text to blue otherwise red
   const colorText = buttonColor === 'red' ? 'blue' : 'red';
 
   const buttonStyle = {
-    backgroundColor: buttonColor
-  }
-  
-  const handleClick = (e) => {
-    e.preventDefault()
+    backgroundColor: disabled ? 'grey' : buttonColor,
+  };
 
-    setButtonColor(colorText)
-  }
+  const handleChange = e => {
+    // console.log(e);
+    // console.log(e.target.checked);
+    setDisabled(e.target.checked);
+  };
+
+  const handleClickBlue = e => {
+    setButtonColor(colorText);
+  };
 
   return (
-    <div className="App">
-      <button style={buttonStyle} onClick={handleClick}>Change to {colorText}</button>
+    <div className='App'>
+      <button style={buttonStyle} onClick={handleClickBlue} disabled={disabled}>
+        Change to {colorText}
+      </button>
+
+      <div>
+        <input
+          type='checkbox'
+          id='disable-button-checkbox'
+          defaultChecked={disabled}
+          aria-checked={disabled}
+          onChange={handleChange}
+        />
+        <label htmlFor='disable-button-checkbox'>Disable button</label>
+      </div>
     </div>
   );
 }
--- color-button-app/src/App.test.js
+++ color-button-app/src/App.test.js
@@ -1,44 +1,99 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import { replaceCamelWithSpaces } from './App';
 
 // screen is global object to access virtual DOM
 
 test('button has correct initial color', () => {
-    // render method creates a virtual DOM for whatever JSX we pass as an argument - App
-    // we want to render App component
-    render(<App />);
+  // render method creates a virtual DOM for whatever JSX we pass as an argument - App
+  // we want to render App component
+  render(<App />);
 
-    // need to find an element 
-    // We are going to use global object Screen that has access to virtual DOM created by render method. 
-    
-    // NOTE: Let's find it by ROLE which is always the preferred method to make sure our app is Accessible. 
-    // NOTE: use getByRole for interactive elements
-    // first arg is the role itself, <a> element has a built in role of 'link'
-    // second arg with NAME OPTION OBJECT to identify what the 'display text' should be for the link
-    // NOTE: This is the way to use actual ROLE to find the element instead of just using the 'text'
-    // & make accessible to users.
+  // need to find an element
+  // We are going to use global object Screen that has access to virtual DOM created by render method.
 
-    // find an element with a role of button and text of 'Change to blue'
-    const colorButton = screen.getByRole('button', { name: /change to blue/i } )
+  // NOTE: Let's find it by ROLE which is always the preferred method to make sure our app is Accessible.
+  // NOTE: use getByRole for interactive elements
+  // first arg is the role itself, <a> element has a built in role of 'link'
+  // second arg with NAME OPTION OBJECT to identify what the 'display text' should be for the link
+  // NOTE: This is the way to use actual ROLE to find the element instead of just using the 'text'
+  // & make accessible to users.
 
-    // NOTE: if not sure, what the element's ROLE suppose to be or Unable to find an accessible element with the role
-    // The test console helps us by giving hints for the ROLE on that particular element. 
+  // find an element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole('button', { name: /change to blue/i });
 
-    // expect the background color to be red
-    // toHaveStyle(css: string | object) - args can be String or Object
-    expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+  // NOTE: if not sure, what the element's ROLE suppose to be or Unable to find an accessible element with the role
+  // The test console helps us by giving hints for the ROLE on that particular element.
 
-    // interacting with DOM
-    // fireEvent object will help us to interact with elements in virtual DOM
-    // to simulate click button - button is being clicked
-    fireEvent.click(colorButton) // first arg is an Element, 
+  // expect the background color to be red
+  // toHaveStyle(css: string | object) - args can be String or Object
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
 
-    // after button is being clicked, expect the background color to be blue
-    expect(colorButton).toHaveStyle({ backgroundColor: 'blue'})
+  // interacting with DOM
+  // fireEvent object will help us to interact with elements in virtual DOM
+  // to simulate click button - button is being clicked
+  fireEvent.click(colorButton); // first arg is an Element,
 
-    // expect the button text to be 'Change to red' after button gets clicked
-    expect(colorButton.textContent).toBe('Change to red')
-  });
+  // after button is being clicked, expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+
+  // expect the button text to be 'Change to red' after button gets clicked
+  expect(colorButton.textContent).toBe('Change to red');
+});
+
+// Button starts out 'enabled' & checkbox start 'unchecked'
+test('initial conditions', () => {
+  render(<App />);
+  // check that the button starts out enabled
+  const colorButton = screen.getByRole('button', { name: 'Change to blue'});
+  expect(colorButton).toBeEnabled();
+
+  // check that the checkbox starts out unchecked
+  const checkbox = screen.getByRole('checkbox');
+  // 'not' is part of jest assertion to negate (change)
+  expect(checkbox).not.toBeChecked();
+})
+
+// when the checkbox is checked, button should be disabled
+test('when the checkbox is checked, button should be disabled and enabled on second click', () => {
+  render(<App />);
+
+  // find button
+  const button = screen.getByRole('button');
+  
+  // find checkbox by it's label, very useful when there are lots of checkbox
+  const checkbox = screen.getByRole('checkbox', { name: 'Disable button'});
+
+  // when checkbox is clicked, button should be disabled
+  // fireEvent to simulate click
+  fireEvent.click(checkbox); 
+  expect(button).toBeDisabled();
+  // on disable, button color is gray
+  expect(button).toHaveStyle({ backgroundColor: 'grey' });
+
+  // Again when checkbox is clicked, button should be enabled
+  fireEvent.click(checkbox); 
+  expect(button).toBeEnabled();
+  // on enable, button color is red
+  expect(button).toHaveStyle({ backgroundColor: 'red' });
+});
+
+// unit testing
+// describe statement - to group tests
+describe('spaces before camel-case capital letters', () => {
+  test('Works for no inner capital letters', () => {
+    expect(replaceCamelWithSpaces('Red')).toBe('Red')
+  })
+
+  test('Works for one inner capital letter', () => {
+    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue')
+  })
+
+  test('Works for multiple inner capital letter', () => {
+    expect(replaceCamelWithSpaces('MediumVioletRed')).toBe('Medium Violet Red')
+  })
+})
+
 
 
 //test('renders learn react link', () => {
