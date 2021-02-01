d77db3bb7b179a5a828f34a8ed0a8f4184eae68a
Rupak Lama <rlama@uptopcorp.com>
2021-02-01 13:18:11 -0500

    asyn assertion to fix act-warning

--- sundae-testing-app/src/pages/summary/test/SummaryForm.test.jsx
+++ sundae-testing-app/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,16 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import {
+  queryByText,
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+
+// user-event tries to simulate the real events that would happen in the browser as the user interacts with it
+// Fire events the same way the user does
+// NOTE: Most projects have a few use cases for 'fireEvent',
+// but the majority of the time you should probably use @testing-library/user-event
+import userEvent from '@testing-library/user-event';
+
 import SummaryForm from '../SummaryForm';
 
 // render method creates a virtual DOM for whatever JSX we pass as an argument
@@ -31,14 +43,53 @@ test('initial conditions', () => {
   const button = screen.getByRole('button', { name: /confirm order/i });
   expect(button).toBeDisabled();
 
-  // fireEvent to simulate click
+  // user-event tries to simulate the real events like click
   // when checkbox is clicked, button should be disabled
-  fireEvent.click(checkbox);
+  userEvent.click(checkbox);
   expect(button).toBeEnabled();
 
   // 3. un-checking checkbox again disables button
-  fireEvent.click(checkbox);
+  userEvent.click(checkbox);
   expect(button).toBeDisabled();
 });
 
 // NOTE: Testing POPOVER to test how element gets disappeared from a page
+
+test('popover response to hover', async () => {
+  render(<SummaryForm />);
+
+  // popover starts out hidden
+  // note: using 'query/queryBy' since element not to be in DOM, Searching by 'text' to find a POPOVER
+  // NOTE: queryBy returns 'null' if no match
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // popover appears upon mouseover/hover of checkbox label
+  // we are going to simulate a mouseover, Searching by 'text' to find a POPOVER
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  // userEvent's hover method - Hovers over element
+  // simulating mouse hover to display popover
+  userEvent.hover(termsAndConditions);
+
+  // check to see if our popover actually appears
+  // this time we will use getBy, we expect an element to be in the DOM
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  // popover disappears when we mouse out
+  // userEvent's un-hover method, simulating mouse unhover
+  userEvent.unhover(termsAndConditions);
+  // note: using 'query' since element not to be in DOM now
+
+  // ERROR: popover is disappearing asynchronously after the above test is complete
+  // https://testing-library.com/docs/guide-disappearance#waiting-for-disappearance
+  // waitForElementToBeRemoved async helper function is what we want
+  // waitFor async helper function retries until the wrapped function stops throwing an error.
+  // This can be used to assert that an element disappears from the page
+  await waitForElementToBeRemoved(() =>
+    // no expect statement here it acts assertion on its own
+    screen.queryByText(/no ice cream will actually be delivered/i)
+  ); // NOTE: We are making our assertion asyn here to solve issue with act-warning
+});
